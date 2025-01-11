9fbd8454dfc45c65adb93c9c79b99d22658f8346
Chiraggohil10~ <gohilchirag90994@gmail.com>
2025-01-11 12:21:44 +0530

    update App component to include headers for State and data fetching sections

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ function App() {
     <div>
       {/* <h1>Explain JSX and Function Calling,Event Handlers</h1>
       <Explainjsx_fcall/> */}
+
       <h1>State in React</h1>
       <State/>
+
       <h1>Fetching Data From API using Function Component</h1>
       <Formating/>
+
       <h1>Fetching Data from API using Class Component</h1>
       <FetchDataFromClassComp/>
+
+      
     </div>
   );
 }
